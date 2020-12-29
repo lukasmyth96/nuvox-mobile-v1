680a7fa8b6ba2aeeb8d29b54d936e849ea76073b
lukasmyth96 <luka@nate.tech>
2020-12-29 14:12:24 +0000

    add signup view

--- nuvox_app/users/urls.py
+++ nuvox_app/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import Login
+from .views import LoginView, SignUpView
 
 urlpatterns = [
-    path('login/', Login.as_view(), name='login'),
+    path('signup/', SignUpView.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     ]
--- nuvox_app/users/views.py
+++ nuvox_app/users/views.py
@@ -1,9 +1,20 @@
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .forms import SignUpForm
 
 
-class Login(auth_views.LoginView):
-
+class LoginView(auth_views.LoginView):
     template_name = 'admin/login.html'
     form_class = AuthenticationForm
     authentication_form = AuthenticationForm
+
+
+class SignUpView(SuccessMessageMixin, CreateView):
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('login')
+    form_class = SignUpForm
+    success_message = "Your profile was created successfully"
