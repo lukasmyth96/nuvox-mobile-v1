914b41b601a197620ea9e2efccf7158bfe10b710
lukasmyth96 <luka@nate.tech>
2020-12-29 14:21:49 +0000

    add boostrap js and css

--- nuvox_app/users/forms.py
+++ nuvox_app/users/forms.py
@@ -7,6 +7,11 @@ from .models import User
 class SignUpForm(UserCreationForm):
     email = forms.EmailField()
 
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        for fieldname in ['username', 'email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1']
+        fields = ['username', 'email', 'password1', 'password2']
