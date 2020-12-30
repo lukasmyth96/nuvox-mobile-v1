ade0cae8be0164d717e62f52c8a8722c23a0175f
lukasmyth96 <luka@nate.tech>
2020-12-30 13:52:38 +0000

    added endpoint for random word

--- nuvox_app/keyboard/urls.py
+++ nuvox_app/keyboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import keyboard
+from .views import keyboard, random_word
 
 urlpatterns = [
     path('keyboard/', keyboard, name='keyboard'),
+    path('random-word/', random_word, name='random-word')
     ]
--- nuvox_app/keyboard/views.py
+++ nuvox_app/keyboard/views.py
@@ -1,6 +1,18 @@
+import random
+
+from english_words import english_words_set
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
 
 
 def keyboard(request):
     context = {'is_mobile': request.user_agent.is_mobile}
     return render(request=request, template_name='keyboard/keyboard.html', context=context)
+
+
+@require_http_methods(['GET'])
+def random_word(request):
+    english_words_list = list(english_words_set)
+    word = random.choice(english_words_list)
+    return JsonResponse({'word': word})
