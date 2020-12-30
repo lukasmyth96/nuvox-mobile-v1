d78bc7fc345e3152261bdcb80a7159e49dd73576
lukasmyth96 <luka@nate.tech>
2020-12-30 16:11:53 +0000

    move submitSwipe function back to js file but set csrfToken to global variable to make this possible

--- nuvox_app/keyboard/static/keyboard/keyboard_ajax.js
+++ nuvox_app/keyboard/static/keyboard/keyboard_ajax.js
@@ -9,3 +9,21 @@ function setNewTargetWord() {
         }
     );
 }
+
+function submitSwipe() {
+    $.ajax({
+            url: '/data-collection-swipes/',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                "target_text": targetText,
+                "trace": trace,
+            }),
+            beforeSend: function(xhr){xhr.setRequestHeader('X-CSRFToken', csrfToken);},
+            dataType: 'json',
+            success: function () {
+                alert('Thanks for submitting a swipe!')
+            }
+        }
+    );
+}
