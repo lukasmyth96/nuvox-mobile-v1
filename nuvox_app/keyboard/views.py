c9962d1744988e911a5179acbb04c4c3ebc4dd1e
lukasmyth96 <luka@nate.tech>
2020-12-30 16:08:45 +0000

    add login required for keyboard view

--- nuvox_app/keyboard/views.py
+++ nuvox_app/keyboard/views.py
@@ -2,6 +2,7 @@ import random
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from wordfreq import top_n_list
@@ -10,6 +11,7 @@ from keyboard.models import DataCollectionSwipe
 from keyboard.serializers import DataCollectionSwipeSerializer
 
 
+@login_required()
 def keyboard(request):
     context = {'is_mobile': request.user_agent.is_mobile}
     return render(request=request, template_name='keyboard/keyboard.html', context=context)
