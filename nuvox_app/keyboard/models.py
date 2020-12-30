038635d6d978803a2550c9acbc0db770e597ae33
lukasmyth96 <luka@nate.tech>
2020-12-30 14:50:41 +0000

    created abstract base class for Swipe model + validator for trace JSONField

--- nuvox_app/keyboard/models.py
+++ nuvox_app/keyboard/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from keyboard.validators import validate_trace
+
+
+class BaseSwipe(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    trace = models.JSONField(validators=[validate_trace])
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        """Overriding to validate the trace before saving."""
+        validate_trace(trace=self.trace)
+        super(BaseSwipe, self).save(
+            force_insert=force_insert,
+            force_update=force_update,
+            using=using,
+            update_fields=update_fields
+        )
---
+++ nuvox_app/keyboard/validators.py
@@ -0,0 +1,29 @@
+from typing import List, Dict
+
+from django.core.exceptions import ValidationError
+
+
+def validate_trace(trace: List[Dict[str, float]]):
+    """Validate that trace is List[Dict[str, float]] where
+    each trace point is a dict with keys 'x', 'y' and 't' that
+    all map to float values."""
+    if not trace:
+        raise ValidationError('Field "trace" cannot be empty')
+
+    if not isinstance(trace, list):
+        raise ValidationError('Field "trace" must be a list')
+
+    for trace_point in trace:
+        validate_trace_point(trace_point)
+
+
+def validate_trace_point(trace_point: Dict[str, float]):
+    if not isinstance(trace_point, dict):
+        raise ValidationError('Each trace point must be a dict')
+
+    if not set(trace_point.keys()) == {'x', 'y', 't'}:
+        raise ValidationError(f'Each trace point have keys x, y, t but found'
+                              f'point with keys: {trace_point}')
+
+    if not all([isinstance(val, float) for val in trace_point]):
+        raise ValidationError(f'Each trace point value must a float')
