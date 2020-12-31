5e678f57113674e17690a8c3b4570c14e236e476
lukasmyth96 <luka@nate.tech>
2020-12-31 16:32:57 +0000

    show good and bad swipes seperately on admin table for game

--- nuvox_app/games/admin.py
+++ nuvox_app/games/admin.py
@@ -6,9 +6,14 @@ from keyboard.models import DataCollectionSwipe
 
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-    list_display = ('user', 'created_on', 'number_of_swipes')
+    list_display = ('user', 'created_on', 'number_of_successful_swipes', 'number_of_unsuccessful_swipes')
 
     @staticmethod
-    def number_of_swipes(obj: Game):
-        swipes_for_this_game = Game.objects.filter(game=obj)
+    def number_of_successful_swipes(obj: Game):
+        swipes_for_this_game = DataCollectionSwipe.objects.filter(game=obj, trace_matches_text=True)
+        return len(swipes_for_this_game)
+
+    @staticmethod
+    def number_of_unsuccessful_swipes(obj: Game):
+        swipes_for_this_game = DataCollectionSwipe.objects.filter(game=obj, trace_matches_text=False)
         return len(swipes_for_this_game)
