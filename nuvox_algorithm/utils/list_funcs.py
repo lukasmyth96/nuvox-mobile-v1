6193afaaf433bb496a9f228829902d4eea524b4f
lukasmyth96 <luka@nate.tech>
2020-12-31 13:14:05 +0000

    added all_subsequences function

--- nuvox_algorithm/utils/list_funcs.py
+++ nuvox_algorithm/utils/list_funcs.py
@@ -1,15 +1,28 @@
-from typing import Iterable, List
+from typing import Iterable, List, Any
 
-from itertools import tee
+import itertools
 
 
 def sequential_pairs(iterable: Iterable):
     """Returns ordered pairs of adjacent items in iterable -
     i.e. s -> (s0, s1), (s1, s2)..."""
-    i, j = tee(iterable)
+    i, j = itertools.tee(iterable)
     next(j, None)
     return zip(i, j)
 
 
 def filter_adjacent_duplicates(a_list: List):
     return [i for i, j in sequential_pairs(a_list + [not a_list[-1]]) if i != j]
+
+
+def all_subsequences(a_list: List) -> List[List[Any]]:
+    """Returns all sub-sequences of a list.
+
+    Examples
+    --------
+    - [1, 2, 1] --> [[1], [2], [1], [1, 2], [1, 1], [2, 1], [1, 2, 1]]
+    """
+    subsequences = []
+    for subsequence_length in range(1, len(a_list) + 1):
+        subsequences += [list(subsequence) for subsequence in (itertools.combinations(a_list, subsequence_length))]
+    return subsequences
