15df0e60a86e430005582c86f1b654ac994146a5
lukasmyth96 <luka@nate.tech>
2020-12-31 16:06:00 +0000

    added game to admin

--- nuvox_app/games/admin.py
+++ nuvox_app/games/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from games.models import Game
+from keyboard.models import DataCollectionSwipe
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_on', 'number_of_swipes')
+
+    @staticmethod
+    def number_of_swipes(obj: Game):
+        swipes_for_this_game = Game.objects.filter(game=obj)
+        return len(swipes_for_this_game)
