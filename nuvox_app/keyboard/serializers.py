1c48ee0c5eaa228de0e97efda0ba7dfa7d991c19
lukasmyth96 <luka@nate.tech>
2020-12-31 17:11:44 +0000

    remove has_expired method field - doesn't make sense

--- nuvox_app/keyboard/serializers.py
+++ nuvox_app/keyboard/serializers.py
@@ -4,14 +4,8 @@ from keyboard.models import DataCollectionSwipe
 
 
 class DataCollectionSwipeSerializer(serializers.ModelSerializer):
-    game_expired = serializers.SerializerMethodField()
 
     class Meta:
         model = DataCollectionSwipe
         fields = ('game', 'target_text', 'trace', 'trace_matches_text')
         read_only_fields = ('trace_matches_text',)
-
-    @staticmethod
-    def get_game_expired(obj: DataCollectionSwipe):
-        """Method for 'game_expired' method field."""
-        return obj.game.has_expired
