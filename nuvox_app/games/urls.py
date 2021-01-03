fcee20a5528c897b1f4b3a3671b71bbe5c66137e
lukasmyth96 <luka@nate.tech>
2021-01-03 15:32:51 +0000

    added leaderboard view

--- nuvox_app/games/urls.py
+++ nuvox_app/games/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from games.views import GameViewSet
+from games.views import GameViewSet, leaderboard
 
 router = routers.DefaultRouter()
 router.register('games', GameViewSet, 'games')
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/leaderboard', leaderboard, name='leaderboard')
 ]
--- nuvox_app/games/views.py
+++ nuvox_app/games/views.py
@@ -1,3 +1,5 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -8,8 +10,17 @@ from games.serializers import GameSerializer
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
-    http_method_names = ['post']
+    http_method_names = ['get', 'post']
     # permission_classes = [IsAuthenticated]  # TODO uncomment after testing
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+@require_http_methods(['GET'])
+def leaderboard(request):
+    games = list(Game.objects.all())
+    games.sort(key=lambda game: len(game.successful_swipes), reverse=True)
+    top_games = games[:5]
+    response = [{'user': game.user.username, 'score': len(game.successful_swipes)} for game in top_games]
+    return JsonResponse(response, safe=False)
