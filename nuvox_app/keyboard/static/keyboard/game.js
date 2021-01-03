54ad6eca785170ed40bbc31335eb85843f579cce
lukasmyth96 <luka@nate.tech>
2021-01-03 13:56:50 +0000

    added simple timer

--- nuvox_app/keyboard/static/keyboard/game.js
+++ nuvox_app/keyboard/static/keyboard/game.js
@@ -2,13 +2,23 @@
 
 function resetTimer() {
     let secondsRemaining = 10;
+    setCountdown(secondsRemaining);
     const downloadTimer = setInterval(function(){
       if(secondsRemaining <= 0){
         clearInterval(downloadTimer);
-        document.getElementById("countdown").innerHTML = "Finished";
-      } else {
-        document.getElementById("countdown").innerHTML = secondsRemaining.toString();
+        onGameOver();
       }
+      setCountdown(secondsRemaining);
       secondsRemaining -= 1;
     }, 1000);
+}
+
+function setCountdown(secondsRemaining) {
+    document.getElementById("countdown").innerHTML = secondsRemaining.toString();
+}
+
+function onGameOver() {
+    gameInProgress = false;
+    gameId = undefined;
+    alert('Game Over!');
 }
