4824f7a38f9306cc6f369e9c4932154725b49377
lukasmyth96 <luka@nate.tech>
2021-01-09 11:56:27 +0000

    added io funcs for read/write json

--- nuvox_algorithm/utils/io_funcs.py
+++ nuvox_algorithm/utils/io_funcs.py
@@ -1,9 +1,25 @@
-def read_text_file(file_path):
+import json
+from typing import Union
+
+
+def read_text_file(file_path: str) -> str:
     with open(file_path, 'r', encoding='utf-8') as text_file:
         data = text_file.read()
     return data
 
 
-def write_text_file(file_path, a_string):
+def write_text_file(file_path: str, a_string: str):
     with open(file_path, 'w', encoding='utf-8') as text_file:
         text_file.write(a_string)
+
+
+def read_json_file(file_path: str) -> Union[list, dict]:
+    with open(file_path, 'r', encoding='utf-8') as json_file:
+        data_dict = json.load(json_file)
+
+    return data_dict
+
+
+def write_json_file(file_path: str, data: Union[list, dict]):
+    with open(file_path, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, sort_keys=True, indent=4)
