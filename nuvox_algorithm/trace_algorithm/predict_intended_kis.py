275446ee2ba54966d634a8071f943c8e61579b37
lukasmyth96 <luka@nate.tech>
2021-01-09 11:14:29 +0000

    added shell for predict_intended_kis

---
+++ nuvox_algorithm/trace_algorithm/predict_intended_kis.py
@@ -0,0 +1,7 @@
+from typing import Dict, List
+
+
+def predict_intended_kis(trace: List[Dict[str, float]]) -> Dict[str, float]:
+    """Returns dictionary mapping key-id-sequence to predicted
+    probability that this was the intended KIS."""
+    pass
