4dfcfd3b2c329a53d28402e49ea7007dfbc37675
lukasmyth96 <luka@nate.tech>
2021-01-15 13:56:08 +0000

    add function to download_dataset.py

---
+++ definition.py
@@ -0,0 +1,7 @@
+import os
+
+REPO_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+TRACE_ALGORITHM_DATASET_PATH = os.path.join(REPO_ROOT_DIR, 'nuvox_algorithm', 'trace_algorithm', 'dataset.json')
+
+KEYBOARD_IMAGE_PATH = os.path.join(REPO_ROOT_DIR, 'nuvox_app', 'keyboard', 'static', 'assets', 'nuvox_keyboard_img.png')
