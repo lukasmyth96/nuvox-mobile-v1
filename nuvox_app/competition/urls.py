9368da91de570a99678f800a657ff62779a5e2e7
lukasmyth96 <luka@nate.tech>
2021-01-16 11:36:17 +0000

    added simple leaderboard template

--- nuvox_app/competition/urls.py
+++ nuvox_app/competition/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from competition.views import competition
+from competition.views import competition, submissions
 
 urlpatterns = [
     path('competition/', competition, name='competition'),
-    ]
+    path('submissions/', submissions, name='submissions')
+]
--- nuvox_app/competition/views.py
+++ nuvox_app/competition/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
+from competition.models import Submission
+
 
 def competition(request):
+    """Homepage for competition."""
     return render(request, template_name='competition/competition.html')
 
+
+def submissions(request):
+    """Submissions leaderboard."""
+    all_submissions = Submission.objects.all()
+    username_accuracy_tuples = [(submission.user.username, submission.top1_accuracy) for submission in all_submissions]
+    ranked_username_accuracy_tuples = sorted(username_accuracy_tuples, key=lambda i: i[1], reverse=True)
+    context = {
+        'ranked_username_accuracy_tuples': ranked_username_accuracy_tuples
+    }
+    return render(request, template_name='competition/submissions.html', context=context)
