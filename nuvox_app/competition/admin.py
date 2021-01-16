01332577e9a1f31646d27d495b018bed300f3e23
lukasmyth96 <luka@nate.tech>
2021-01-16 11:24:28 +0000

    add Submission to admin

--- nuvox_app/competition/admin.py
+++ nuvox_app/competition/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from competition.models import Submission
+
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'description', 'top1_accuracy', 'top3_accuracy')
