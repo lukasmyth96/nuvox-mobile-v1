96b2b24e49621d3b5ace1a62c5d18b202b70268a
lukasmyth96 <luka@nate.tech>
2021-01-16 10:42:27 +0000

    added Submission model + migrated

--- nuvox_app/competition/models.py
+++ nuvox_app/competition/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+from users.models import User
+
+
+class Submission(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='submissions')
+    description = models.TextField(null=True, blank=True)
+    predictions = models.JSONField()
+    top1_accuracy = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(1.0)])
+    top3_accuracy = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(1.0)])
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
