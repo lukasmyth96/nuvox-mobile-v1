3cf8057e253ad1da897441479a3fa10781fdaa86
lukasmyth96 <luka@nate.tech>
2021-01-17 13:00:07 +0000

    add submit view to urls

--- nuvox_app/competition/urls.py
+++ nuvox_app/competition/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from competition.views import competition, submissions
+from competition.views import competition, submissions, SubmissionCreateView
 
 urlpatterns = [
     path('competition/', competition, name='competition'),
-    path('competition/submissions/', submissions, name='submissions')
+    path('competition/submissions/', submissions, name='submissions'),
+    path('competition/submit/', SubmissionCreateView.as_view(), name='submit'),
 ]
