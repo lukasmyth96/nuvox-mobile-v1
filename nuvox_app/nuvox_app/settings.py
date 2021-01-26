c4c26983bffa6cb5ef7b862c17903da6686684d0
lukasmyth96 <luka@nate.tech>
2021-01-26 20:49:24 +0000

    added health check middleware

--- nuvox_app/nuvox_app/settings.py
+++ nuvox_app/nuvox_app/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'nuvox_app.middleware.HealthCheckMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
