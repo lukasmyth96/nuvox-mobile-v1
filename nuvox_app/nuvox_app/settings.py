4d72a0816646fb905049cc62f05dc5aef2336d38
lukasmyth96 <luka@nate.tech>
2021-01-29 15:08:52 +0000

    add static directory

--- nuvox_app/nuvox_app/settings.py
+++ nuvox_app/nuvox_app/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / '../www/static'
+STATIC_ROOT = 'static'
 
 AUTH_USER_MODEL = 'users.User'
 
