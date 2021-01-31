3faefdd86853439454249824d8bd934c27292712
lukasmyth96 <luka@nate.tech>
2021-01-31 15:09:27 +0000

    added test_generate_competition_submission.py

---
+++ nuvox_algorithm/tests/test_trace_algorithm/test_generate_competition_submission.py
@@ -0,0 +1,41 @@
+from nuvox_algorithm.utils.io_funcs import read_json_file
+from nuvox_algorithm.trace_algorithm import TraceAlgorithm, Swipe, TracePoint
+from nuvox_algorithm.trace_algorithm.generate_competition_submission import main_generate_competition_submission
+
+
+def test_main_generate_competition_submission(tmpdir):
+
+    output_path = str(tmpdir / 'submission.json')
+
+    swipes = [
+        Swipe(
+            id=1,
+            user_id=1,
+            device_type='pc',
+            trace=[
+                TracePoint(x=0.1, y=0.1, t=0.1, key_id='1')
+            ],
+            target_text='hello',
+            target_kis='3246'
+        )
+    ]
+
+    trace_algorithm = TraceAlgorithm()
+    trace_algorithm.predict_intended_kis = lambda trace: {'3246': 1.0}
+
+    main_generate_competition_submission(
+        swipes=swipes,
+        trace_algorithm=trace_algorithm,
+        output_path=output_path
+    )
+
+    generated_submission = read_json_file(output_path)
+
+    assert isinstance(generated_submission, list)
+    assert len(generated_submission) == 1
+    assert generated_submission[0] == {
+        'id': 1,
+        'prediction': {
+            '3246': 1.0
+        }
+    }
--- nuvox_algorithm/trace_algorithm/generate_competition_submission.py
+++ nuvox_algorithm/trace_algorithm/generate_competition_submission.py
@@ -1,23 +1,18 @@
+from typing import List
+
 from tqdm import tqdm
 
 from definition import TRACE_ALGORITHM_SUBMISSION_PATH
 from nuvox_algorithm.utils.io_funcs import write_json_file
 from nuvox_algorithm.trace_algorithm.utils import load_test_set
-from nuvox_algorithm.trace_algorithm.trace_algorithm import TraceAlgorithm
+from nuvox_algorithm.trace_algorithm import TraceAlgorithm, Swipe
 
 
-if __name__ == '__main__':
-    """
-    This script uses your trace algorithm to perform predictions for the
-    competition test set and saves them to a 'submission.json' file which
-    you can then upload on the nuvox website.
-    """
-    # Load dataset of Swipes from data dump JSON file.
-    swipes = load_test_set()
-
-    # Instantiate your TraceAlgorithm here.
-    trace_algorithm = TraceAlgorithm()
-
+def main_generate_competition_submission(swipes: List[Swipe],
+                                         trace_algorithm: TraceAlgorithm,
+                                         output_path: str) -> List[dict]:
+    """Performs predictions on list of Swipes, saves predictions to
+    JSON file and also returns them."""
     predictions = []
     for swipe in tqdm(swipes):
 
@@ -30,5 +25,27 @@ if __name__ == '__main__':
             }
         )
 
-    write_json_file(file_path=TRACE_ALGORITHM_SUBMISSION_PATH, data=predictions)
+    write_json_file(file_path=output_path, data=predictions)
+
+    return predictions
+
+
+if __name__ == '__main__':
+    """
+    This script uses your trace algorithm to perform predictions for the
+    competition test set and saves them to a 'submission.json' file which
+    you can then upload on the nuvox website.
+    """
+    # Load dataset of Swipes from data dump JSON file.
+    _swipes = load_test_set()
+
+    # Instantiate your TraceAlgorithm here.
+    _trace_algorithm = TraceAlgorithm()
+
+    main_generate_competition_submission(
+        swipes=_swipes,
+        trace_algorithm=_trace_algorithm,
+        output_path=TRACE_ALGORITHM_SUBMISSION_PATH
+    )
+
     print(f'Predictions saved to {TRACE_ALGORITHM_SUBMISSION_PATH}')
