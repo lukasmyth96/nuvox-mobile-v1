15594228ac2ff79d09a6a69c712444e4b2ef6427
lukasmyth96 <luka@nate.tech>
2021-02-05 09:08:41 +0000

    add links from login to signup and back

--- nuvox_app/users/urls.py
+++ nuvox_app/users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import LoginView, SignUpView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     ]
