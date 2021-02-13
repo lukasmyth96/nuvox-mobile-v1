17fd5b4f794656d0a4b3a0e822315694443ded65
lukasmyth96 <luka@nate.tech>
2021-02-13 16:14:57 +0000

    tidied up angle.py

--- nuvox_algorithm/trace_algorithm/utils/angle.py
+++ nuvox_algorithm/trace_algorithm/utils/angle.py
@@ -1,21 +1,32 @@
 import numpy as np
 
 
-def angle(vector):
+def angle(vectors: np.ndarray) -> np.ndarray:
+    """Returns the angles between vectors.
+
+    Parameters
+    ------------
+    vectors: np.ndarray
+        A 2D-array of shape (N,M) representing N vectors in M-dimensional space.
+
+    Returns
+    ------------
+    angles: np.ndarray
+        A 1D-array of values of shape (N-1,), with each value between 0 and pi where:
+            - 0 implies the vectors point in the same direction
+            - pi/2 implies the vectors are orthogonal
+            - pi implies the vectors point in opposite directions
     """
-    Returns the angles between vectors.
 
-    Parameters:
-    dir is a 2D-array of shape (N,M) representing N vectors in M-dimensional space.
+    # For the point at index i, vectors_inbound[i] is the (x, y) vector
+    # inbound to that point and vectors_outbound[i] is the vector outbound.
+    vectors_inbound = vectors[:-1]
+    vectors_outbound = vectors[1:]
 
-    The return value is a 1D-array of values of shape (N-1,), with each value
-    between 0 and pi.
+    # Using law of cosines: θ = arcos(a•b / |a||b|)
+    inbound_dot_outbound = (vectors_inbound * vectors_outbound).sum(axis=1)  # a•b
+    mod_inbound_mod_outbound = (np.sqrt((vectors_inbound ** 2).sum(axis=1) * (vectors_outbound ** 2).sum(axis=1)))  # |a||b|
+    cosine_angles = inbound_dot_outbound / mod_inbound_mod_outbound  # cos(θ)
+    angles = np.arccos(cosine_angles)
 
-    0 implies the vectors point in the same direction
-    pi/2 implies the vectors are orthogonal
-    pi implies the vectors point in opposite directions
-    """
-    dir2 = vector[1:]
-    dir1 = vector[:-1]
-    return np.arccos((dir1*dir2).sum(axis=1)/(
-        np.sqrt((dir1**2).sum(axis=1)*(dir2**2).sum(axis=1))))
+    return angles
