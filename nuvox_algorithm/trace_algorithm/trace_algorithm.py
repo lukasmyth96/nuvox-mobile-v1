cab80f3880199fa1494aef7582a4109eb49c38c4
lukasmyth96 <luka@nate.tech>
2021-02-13 15:35:57 +0000

    tidy up trace_algorithm.py

--- nuvox_algorithm/trace_algorithm/trace_algorithm.py
+++ nuvox_algorithm/trace_algorithm/trace_algorithm.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Tuple, Optional
 
 from matplotlib import pyplot as plt
 import numpy as np
@@ -10,51 +10,40 @@ from nuvox_algorithm.trace_algorithm.utils import rdp, angle
 
 class TraceAlgorithm:
     def __init__(self, rdp_threshold: float, angle_threshold: float):
-        # The init method can be used to load a machine learning model or simply
-        # set some configurations for your trace algorithm. You do not have to
-        # use it if you do not need it.
         self.rdp_threshold = rdp_threshold
         self.angle_threshold = angle_threshold
 
-    def predict_intended_kis(self, trace: List[TracePoint]) -> Dict[str, float]:
-        """This method receives a swipe trace (chronological list
-        of TracePoint objects) and must return a dictionary mapping possible
-        key-id-sequences (KIS) to the predicted probability that the user
-        intended to swipe that sequence of keys.
+    def predict_intended_kis(self,
+                             trace: List[TracePoint],
+                             plot: Optional[bool] = False) -> Dict[str, float]:
+        """This method receives a swipe trace and must return a dictionary
+        mapping possible key-id-sequences (KIS) to the predicted probability
+        that the user intended to swipe that sequence of keys.
 
         Notes
         --------
-        - The values (predicted probabilities) in the returned
-        dictionary must be sum to 1.0 as the dictionary must describe
-        a probability distribution.
+        - The values in the returned dictionary must be sum to 1.0 as
+        the dictionary must describe a probability distribution.
 
-        Example
+        Returns
         --------
-        - If a user attempts to swipe the word 'beg', their trace will
-        presumably pass over keys 1 --> 2 --> 3.
-        - In this case the true intended KIS would be '123' because
-        the word 'beg' requires 'b' from key 1, 'e' from key 2 and 'g' from key 3.
-        - For this input trace, this function may, for example, return:
-          {
-            '123': 0.8,
-            '13': 0.2
-          }
-          thereby assigning a probability of 0.8 that the user intended the KIS
-          1->2->3 and probability of 0.2 that the user only intended 1->3.
+        kis_to_predicted_probability: Dict[str, float]
+            Mapping from a possible key-id-sequence (e.g. '3246') to the predicted
+            probability that the user intended to swipe that sequence of keys.
         """
 
         # All points as (x, y) tuples
         points = [(point.x, point.y) for point in trace]
 
-        # Simplified path using Ramer–Douglas–Peucker algorithm algorithm
+        # Simplified path using Ramer–Douglas–Peucker algorithm
         rdp_points = rdp(points, self.rdp_threshold)
 
         # Array containing (Δx, Δy) vectors for each segment of path.
-        vectors = np.diff(np.array(rdp_points), axis=0)
+        path_vectors = np.diff(np.array(rdp_points), axis=0)
 
         # The angle between each successive segment in the path.
         # Note there is only n-2 angles for n points as you can't calculate angle for first and last.
-        angles = angle(vectors)
+        angles = angle(path_vectors)
 
         # Find the points in the rdp_points list where the angle at that point exceeds the angle threshold
         turning_point_indices = np.where(angles > self.angle_threshold)[0] + 1
@@ -76,14 +65,30 @@ class TraceAlgorithm:
         # Filter out '5' as this key contains no characters so can't have been intended.
         key_ids = [key_id for key_id in key_ids if key_id != '5']
 
-        # Convert list to string i.e. [1, 2, 3] --> '123'
+        # Convert list to string i.e. [1, 2, 3] --> '123' (lists cannot be keys of a dictionary)
         kis = ''.join(key_ids)
 
         kis_to_predicted_probability = {
             kis: 1.0
         }
 
-        # Uncomment this to plot the path and turning points...
+        if plot:
+            self.plot_prediction(
+                points=points,
+                rdp_points=rdp_points,
+                turning_point_indices=turning_point_indices,
+                predicted_kis=kis
+            )
+
+        return kis_to_predicted_probability
+
+    @staticmethod
+    def plot_prediction(points: List[Tuple[float, float]],
+                        rdp_points: List[Tuple[float, float]],
+                        turning_point_indices: List[int],
+                        predicted_kis: str) -> None:
+        """Plots path, simplified (RDP) path and turning points."""
+
         fig = plt.figure()
         ax = fig.add_subplot(111)
 
@@ -99,6 +104,5 @@ class TraceAlgorithm:
         ax.plot(rdp_x[turning_point_indices], rdp_y[turning_point_indices], 'ro', markersize=10, label='turning points')
         ax.invert_yaxis()
         plt.legend(loc='best')
+        plt.title(f'Predicted KIS: {predicted_kis}')
         plt.show()
-
-        return kis_to_predicted_probability
