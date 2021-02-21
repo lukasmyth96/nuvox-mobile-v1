386944cbfa08b2624b2ab962274af7cecbbf5340
lukasmyth96 <luka@nate.tech>
2021-02-21 12:16:00 +0000

    add skeleton keyboard.js

---
+++ nuvox_app/keyboard/static/keyboard/keyboard.js
@@ -0,0 +1,10 @@
+/*
+    The functions in this file are responsible for handling the swipes:
+    - fetching predictions
+    - displaying suggestions
+    - handling clicks on suggestions etc...
+ */
+
+function handleSwipe(trace){
+
+}
